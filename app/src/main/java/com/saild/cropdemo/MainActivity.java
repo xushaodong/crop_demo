d6349da21fde507b6ee864db020f37d4771b1147
shaodong <zijingyuanyuan@163.com>
2019-04-26 17:08:54 +0800

    更新了错误弹窗

--- app/src/main/java/com/saild/cropdemo/MainActivity.java
+++ app/src/main/java/com/saild/cropdemo/MainActivity.java
@@ -110,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Uri uri = Crop.getOutput(result);
             mImageView.setImageURI(uri);
         } else if (resultCode == Crop.RESULT_ERROR) {
-            Toast.makeText(this, "result", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "result_error", Toast.LENGTH_SHORT).show();
         }
     }
 }
